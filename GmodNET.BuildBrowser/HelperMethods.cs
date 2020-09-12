6ddbec36cba76bc27afc04b326107a9eb3857956
GlebChili <gleb.chili@gmail.com>
2020-09-12 21:31:30 +0300

    It works

---
+++ GmodNET.BuildBrowser/HelperMethods.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GmodNET.BuildBrowser
+{
+    public static class HelperMethods
+    {
+        public static NameValueCollection QueryString(this NavigationManager navigationManager)
+        {
+            return HttpUtility.ParseQueryString(new Uri(navigationManager.Uri).Query);
+        }
+
+        public static string GetWebTime(this DateTime dateTime)
+        {
+            var time_since = DateTime.UtcNow - dateTime;
+
+            string ret = dateTime.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+            if(time_since.Days <= 1)
+            {
+                if(time_since.Days == 1)
+                {
+                    ret += " (Yesterday)";
+                }
+                else
+                {
+                    if(time_since.Hours > 1)
+                    {
+                        ret += " (" + time_since.Hours + " hours ago)";
+                    }
+                    else if (time_since.Hours == 1)
+                    {
+                        ret += " (an hour ago)";
+                    }
+                    else
+                    {
+                        if(time_since.Minutes > 1)
+                        {
+                            ret += " (" + time_since.Minutes + " minutes ago)";
+                        }
+                        else
+                        {
+                            ret += " (just now)";
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+    }
+}
--- GmodNET.BuildBrowser/Package.cs
+++ GmodNET.BuildBrowser/Package.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Semver;
 
 namespace GmodNET.BuildBrowser
 {
     public class Package
     {
-        readonly List<PackageVersion> versions;
+        readonly SortedSet<PackageVersion> versions;
         readonly string name;
 
-        public List<PackageVersion> Versions
+        public IEnumerable<PackageVersion> Versions
         {
             get { return versions; }
         }
@@ -21,10 +23,28 @@ namespace GmodNET.BuildBrowser
             get { return name; }
         }
 
+        public DateTime LastUpdate
+        {
+            get
+            {
+                DateTime time = versions.FirstOrDefault().UploadTime;
+
+                foreach(PackageVersion v in versions)
+                {
+                    if(v.UploadTime > time)
+                    {
+                        time = v.UploadTime;
+                    }
+                }
+
+                return time;
+            }
+        }
+
         public Package(string name)
         {
             this.name = name;
-            versions = new List<PackageVersion>();
+            versions = new SortedSet<PackageVersion>(new VersionComparer());
         }
 
         public void AddVersion(string version, string download_path, string upload_date)
@@ -34,6 +54,17 @@ namespace GmodNET.BuildBrowser
                 DateTime uploadDate = DateTime.ParseExact(upload_date, @"yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture);
 
                 versions.Add(new PackageVersion(this, version, download_path, uploadDate));
+            }
+        }
+
+        class VersionComparer : IComparer<PackageVersion>
+        {
+            public int Compare(PackageVersion x, PackageVersion y)
+            {
+                SemVersion x_ver = SemVersion.Parse(x.VersionString);
+                SemVersion y_ver = SemVersion.Parse(y.VersionString);
+
+                return -x_ver.CompareByPrecedence(y_ver);
             }
         }
     }
