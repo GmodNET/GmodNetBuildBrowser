29cff9d81977d9c34e7878809f4ea5bce64197a6
Gleb Krasilich <gleb.chili@gmail.com>
2021-07-23 05:54:33 +0300

    Remove

--- GmodNET.BuildBrowser/PackageVersion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GmodNET.BuildBrowser
-{
-    public record PackageVersion(Package PackageID, string VersionString, string DownloadPath, DateTime UploadTime);
-}
