6b22e3c59fc1625130a4f7b427a2b174ee37ead6
Gleb Krasilich <gleb.chili@gmail.com>
2021-08-30 17:08:48 +0300

    Added working refresh button to Gmod.NET Runtime list

--- Components/RefreshButton.razor.cs
+++ Components/RefreshButton.razor.cs
@@ -1,5 +1,26 @@
-﻿namespace GmodNetBuildBrowser.Components;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace GmodNetBuildBrowser.Components;
 
 public partial class RefreshButton
 {
+    [EditorRequired]
+    [Parameter]
+    public Func<Task> OnClick { get; set; }
+
+    bool isDisabled = false;
+
+    async Task OnClickHandler()
+    {
+        isDisabled = true;
+
+        if (OnClick is not null)
+        {
+            await OnClick();
+        }
+
+        isDisabled = false;
+    }
 }
