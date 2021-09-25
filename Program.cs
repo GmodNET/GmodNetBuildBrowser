d76934aa73dae2c9b54a525a6655b53cd4f4226b
Gleb Krasilich <gleb.chili@gmail.com>
2021-09-26 01:16:52 +0300

    List of GmodNET Runtime builds is now cached in memory

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Octokit;
+using GmodNetBuildBrowser.Services;
 
 namespace GmodNetBuildBrowser
 {
@@ -20,6 +17,7 @@ namespace GmodNetBuildBrowser
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<GitHubClient>(sp => new GitHubClient(new ProductHeaderValue("gmodnet-nightly-builds-browser")));
+            builder.Services.AddSingleton<GmodNetRuntimeVersionsProvider>();
 
             await builder.Build().RunAsync();
         }
---
+++ Services/GmodNetRuntimeVersionsProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Octokit;
+using Semver;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace GmodNetBuildBrowser.Services
+{
+    public class GmodNetRuntimeVersionsProvider
+    {
+        GitHubClient gitHubClient;
+        ReadOnlyCollection<Release> releases;
+
+        public event Action StateChanged;
+
+        public ReadOnlyCollection<Release> Releases
+        {
+            get {  return releases; }
+        }
+
+        public GmodNetRuntimeVersionsProvider(IServiceScopeFactory scope_factory)
+        {
+            gitHubClient = scope_factory.CreateScope().ServiceProvider.GetRequiredService<GitHubClient>();
+        }
+
+        public async Task FetchReleasesAsync()
+        {
+            releases = null;
+            StateChanged();
+
+            List<Release> tmp_releases = (await gitHubClient.Repository.Release.GetAll("GmodNET", "runtime-nightly")).ToList();
+
+            tmp_releases.Sort((x, y) =>
+            {
+                return -SemVersion.Compare(SemVersion.Parse(x.TagName), SemVersion.Parse(y.TagName));
+            });
+
+            releases = tmp_releases.AsReadOnly();
+            StateChanged();
+        }
+    }
+}
