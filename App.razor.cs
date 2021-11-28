12a2bb4e1d83fcf7e841815322ff312eb37edcc8
Gleb Krasilich <gleb.chili@gmail.com>
2021-11-28 14:13:45 +0300

    Added version handling logic

---
+++ App.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using GmodNET.VersionTool.Info;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace GmodNetBuildBrowser
+{
+    public partial class App
+    {
+        [Inject]
+        ILocalStorageService localStorage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Console.WriteLine("GmodNET Nightly Builds Browser\n" +
+                              $"Version {BuildInfo.FullVersion}");
+
+            if (!(await localStorage.ContainKeyAsync("AppVersion")) || 
+                (await localStorage.GetItemAsync<string>("AppVersion")) != BuildInfo.FullVersion)
+            {
+                await localStorage.ClearAsync();
+                await localStorage.SetItemAsync("AppVersion", BuildInfo.FullVersion);
+            }
+        }
+    }
+}
